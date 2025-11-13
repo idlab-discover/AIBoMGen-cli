1d0c8c190cbe19a5e1d11f00f783456bea3d0ce1
wievdndr <wiebe.vandendriessche@ugent.be>
2025-11-13 11:27:19 +0100

    init cra version

--- aibomgen-platform/worker/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Dynamically set the worker name using the Docker Swarm task slot
-WORKER_NAME="worker_${HOSTNAME}"
-
-# Start the Celery worker with the dynamically generated name
-celery -A tasks worker --loglevel=info -n "$WORKER_NAME" --queues=training_queue
